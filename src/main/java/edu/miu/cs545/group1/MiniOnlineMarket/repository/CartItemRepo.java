f72abd61641fa90b3a01e140eb3ca5ceb9230182
CitizenOneEt <87533861+CitizenOneEt@users.noreply.github.com>
2021-12-11 11:34:14 -0600

    set up the repo interfaces (#5)
    * set up the repo interfaces

---
+++ src/main/java/edu/miu/cs545/group1/MiniOnlineMarket/repository/CartItemRepo.java
@@ -0,0 +1,9 @@
+package edu.miu.cs545.group1.MiniOnlineMarket.repository;
+
+import edu.miu.cs545.group1.MiniOnlineMarket.domain.Cart;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CartItemRepo extends JpaRepository<Cart,Long> {
+}
