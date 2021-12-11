5db8f12700e2ebfd01b8f53589a2f9d8858e70c4
James <jpkatarikawe@gmail.com>
2021-12-10 20:50:32 -0600

    unblock h2-console

--- src/main/java/edu/miu/cs545/group1/MiniOnlineMarket/config/SecurityConfig.java
+++ src/main/java/edu/miu/cs545/group1/MiniOnlineMarket/config/SecurityConfig.java
@@ -8,7 +8,11 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 public class SecurityConfig  extends WebSecurityConfigurerAdapter {
 
     @Override
-    protected void configure(HttpSecurity http) throws Exception {
-        http.csrf().disable().authorizeRequests().anyRequest().permitAll();
+    protected void configure(HttpSecurity httpSecurity) throws Exception {
+        httpSecurity.authorizeRequests().antMatchers("/").permitAll().and()
+                .authorizeRequests().antMatchers("/h2-console/**").permitAll();
+        httpSecurity.csrf().disable();
+        httpSecurity.headers().frameOptions().disable();
+
     }
 }
